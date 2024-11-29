1f168317337c547c0d4d1aa5ba401f5c8916361a
AspaNeuro <a.almeida@neurogears.org>
2024-11-29 02:23:15 +0000

    Change to work with a single image and ID
    Co-authored-by: neuroThom <t.ryan@neurogears.org>

--- Workflows/Extensions/IdColorVisualizer.cs
+++ Workflows/Extensions/IdColorVisualizer.cs
@@ -21,22 +21,35 @@ public class IdColorVisualizer
         CV.CvtColor(hsv, rgb, ColorConversion.Hsv2Bgr);
         return rgb[0];
     }
-    public IObservable<IplImage> Process(IObservable<IplImage> source)
+    public IObservable<IplImage> Process(IObservable<Tuple<int,IplImage>> source)
     {
         return source.Select(value => 
         {
-            var output = value.Clone();
-            //var image = value.Item1;
-            //var output = new IplImage(image.Size,image.Depth, 3);
-            //CV.CvtColor(image, output,ColorConversion.Gray2Bgr);
+            var id = value.Item1;
+ 
+            var output = value.Item2.Clone();
+
             
             int colorStep = 180/NumberOfIds;
-            int coordStepY = output.Height/NumberOfIds;
-            for (int i = 0; i < NumberOfIds; i++)
-            {
-                //CV.Circle(output, new Point(value.Item2[i]), RadiusSize, ScalarHSV2BGR(i*step,255,255), 2);
-                CV.Circle(output, new Point(5,(i*coordStepY)+5), 4, ScalarHSV2BGR(i*colorStep,255,255), -1);
-            }
+            CV.Circle(output, new Point(5,5), 4, ScalarHSV2BGR(id*colorStep,255,255), -1);
+
+            
+            return output;
+
+        });
+    }
+    public IObservable<IplImage> Process(IObservable<Tuple<IplImage,int>> source)
+    {
+        return source.Select(value => 
+        {
+            var id = value.Item2;
+ 
+            var output = value.Item1.Clone();
+
+            
+            int colorStep = 180/NumberOfIds;
+            CV.Circle(output, new Point(5,5), 4, ScalarHSV2BGR(id*colorStep,255,255), -1);
+
             
             return output;
 
